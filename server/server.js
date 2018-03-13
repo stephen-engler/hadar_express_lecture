2908b7ae7aa13108d1e049bbd4d2ee046dd37563
Stephen Engler <sengler1989@gmail.com>
2018-03-13 16:43:25 -0500

    added html, client, style

--- server/server.js
+++ server/server.js
@@ -5,9 +5,15 @@ let express = require('express');// this is unique to node modules, don't need .
 //run express function and it returns something to app
 let app = express();
 
+const PORT = 5000;
+
+
 //express lets us slow stuff down 
 //need to tell server what to send
 //control c turns off the server
-app.listen(5000, function(){
+//server our files
+app.use(express.static('server/public'));
+
+app.listen(PORT, ()=>{
     console.log('app is running on port 50000');
 });
